60286f13d156a1e67a0a3aa2724a5cbf89fd52a8
zongxinc <zongxinc@bu.edu>
2020-02-02 13:23:53 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,14 +3,30 @@ import requests
 import csv
 from pprint import pprint
 
-city = input('enter the city name: ')
+airport_name = input('enter the airport name: ')
+
+city = ''
+
+with open('airports.csv', 'r') as airportsfile:
+
+	airportsreader = csv.reader(airportsfile, delimiter = ',')
+
+	for row in airportsreader:
+
+		if (row[3] == airport_name and row[10] != ''):
+
+			city = row[10]
+
+			break
+
+#print(city)
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=0c45f909abe580009e821ecdb47cfa43&units=metric'.format(city)
 
-with open('airports.csv', 'r') as airportsfile:
-	airportsreader = csv.reader()
 res = requests.get(url)
 
 data = res.json()
 
-pprint(data)
+temp = data['main']['temp']
+
+print(airport_name + ' current temperature: ' + str(temp))
