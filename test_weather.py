f8f83215b46119930407dc18ba6f9c878849af2b
zongxinc <zongxinc@bu.edu>
2020-02-04 20:00:50 -0500

    test implement
    test implemented and improve the structure of weather.py

---
+++ test_weather.py
@@ -0,0 +1,7 @@
+from weather import weather
+
+def test()
+	assert weather('York Airport') != 'no airport found'
+	assert weather('jkhkzdhkfhds') == 'no airport found'
+	assert weather('Hammer Airport') != 'no airport found'
+	
--- weather.py
+++ weather.py
@@ -6,58 +6,66 @@ from pprint import pprint
 
 airport_name = input('enter the airport name: ')
 
+def weather(airport_name):
 
-with open('airports.csv', 'r') as airportsfile:
+	latitude = 0
 
-	airportsreader = csv.reader(airportsfile, delimiter = ',')
+	longtitude = 0
 
-	for row in airportsreader:
+	with open('airports.csv', 'r') as airportsfile:
 
-		if row[3] == airport_name:
+		airportsreader = csv.reader(airportsfile, delimiter = ',')
 
-			latitude = row[4]
+		for row in airportsreader:
 
-			longtitude = row[5]
+			if row[3] == airport_name:
+
+				latitude = row[4]
+
+				longtitude = row[5]
+
+				break
+
+		if (longtitude == 0 and latitude == 0):
+
+			return 'no airport found'
+
+	url = 'https://api.weather.gov/points/' + str(latitude) + ',' + str(longtitude)
+
+	res = requests.get(url)
+
+	data = res.json()
+
+	myurl = data['properties']['observationStations']
+
+	res = requests.get(myurl)
+
+	data = res.json()
+
+	observationurl = data['features'][0]['id'] + '/observations'
+
+	res = requests.get(observationurl)
+
+	data = res.json()
+
+	return data['features']
+
+count = 1
+
+if weather(airport_name) == 'no airport found':
+	
+	print('no airport found')
+
+else:
+
+	for elements in weather(airport_name):
+
+		print('date: ' + elements['properties']['timestamp'][0:10] + '  ' + 'time: '+ elements['properties']['timestamp'][11:16] + '  ' + 'temperature: ' + str(math.trunc(elements['properties']['temperature']['value'])) + ' Celsius')
+
+		count = count + 1
+
+		if count > 24:
 
 			break
 
-#print(city)
-
-#url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=0c45f909abe580009e821ecdb47cfa43&units=metric'.format(city)
-
-url = 'https://api.weather.gov/points/' + str(latitude) + ',' + str(longtitude)
-
-res = requests.get(url)
-
-data = res.json()
-
-myurl = data['properties']['observationStations']
-
-res = requests.get(myurl)
-
-data = res.json()
-
-observationurl = data['features'][0]['id'] + '/observations'
-
-res = requests.get(observationurl)
-
-data = res.json()
-
-#print(data['features'])
-count = 1
-
-for elements in data['features']:
-
-	print('date: ' + elements['properties']['timestamp'][0:10] + '  ' + 'time: '+ elements['properties']['timestamp'][11:16] + '  ' + 'temperature: ' + str(math.trunc(elements['properties']['temperature']['value'])) + ' Celsius')
-
-	count = count + 1
-
-	if count > 24:
-
-		break
-
-#pprint(data)
-
-#temp = data['main']['temp']
-
-#print(airport_name + ' current temperature: ' + str(temp))
+#return 1
