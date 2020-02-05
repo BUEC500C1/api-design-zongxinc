b8cb941eb1d5460913960ab2b8ac246a989e3a46
zongxinc <zongxinc@bu.edu>
2020-02-04 20:01:52 -0500

    Update test_weather.py

--- test_weather.py
+++ test_weather.py
@@ -1,6 +1,6 @@
 from weather import weather
 
-def test()
+def test():
 	assert weather('York Airport') != 'no airport found'
 	assert weather('jkhkzdhkfhds') == 'no airport found'
 	assert weather('Hammer Airport') != 'no airport found'
