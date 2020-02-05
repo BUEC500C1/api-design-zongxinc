68f50f4bf699261db29c9c3e9ec284df09c78139
zongxinc <zongxinc@bu.edu>
2020-02-04 20:11:55 -0500

    Update test_weather.py

--- test_weather.py
+++ test_weather.py
@@ -1,7 +1,10 @@
 from weather import weather
 
 def test():
+	weather.input() = lambda: 'York Airport'
 	assert weather('York Airport') != 'no airport found'
+	weather.input() = lambda: 'jkhkzdhkfhds'
 	assert weather('jkhkzdhkfhds') == 'no airport found'
+	weather.input() = lambda: 'Hammer Airport'
 	assert weather('Hammer Airport') != 'no airport found'
 	
